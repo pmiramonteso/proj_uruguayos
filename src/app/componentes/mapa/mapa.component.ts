5520a5c0250ba454a9edd245d4a20c911a33908c
Paola Miramontes <pmiramonteso@gmail.com>
2024-11-18 18:44:06 +0100

    añado mapa

--- src/app/componentes/mapa/mapa.component.ts
+++ src/app/componentes/mapa/mapa.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
+import * as L from 'leaflet';
+import { NegociosService } from '../../service/negocios.service';
 
 @Component({
   selector: 'app-mapa',
@@ -7,6 +9,33 @@ import { Component } from '@angular/core';
   templateUrl: './mapa.component.html',
   styleUrl: './mapa.component.scss'
 })
-export class MapaComponent {
+export class MapaComponent implements OnInit, AfterViewInit {
+  map: any;
+  negocios: any[] = []; 
+  constructor(private negociosService: NegociosService) {}
 
+  ngOnInit(): void {
+  }
+  
+  ngAfterViewInit(): void {
+    this.map = L.map('map').setView([40.4637, -3.7492],6)
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(this.map);
+
+    const customIcon = L.icon({
+      iconUrl: 'https://unpkg.com/leaflet/dist/images/marker-icon.png',
+      iconSize: [25, 41],
+      iconAnchor: [12, 41],
+      popupAnchor: [1, -34],
+    });
+
+    L.marker([40.4637, -3.7492], { icon: customIcon }).addTo(this.map)
+      .bindPopup('¡Hola, este es un marcador!')
+      .openPopup();
+
+      this.map.whenReady(() => {
+        this.map.invalidateSize();
+      })
+        
+  }
 }
