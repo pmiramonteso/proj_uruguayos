54f0d95b13f762e4d22245f3097865ab48c12dcf
Paola Miramontes <pmiramonteso@gmail.com>
2025-02-14 16:10:59 +0100

    separacion de proyecto

--- backend/src/utils/utils.js
+++
@@ -1,9 +0,0 @@
-function esPar(n) {
-    return n % 2 === 0;
-}
-
-function contraseniasCoinciden(password, confirmPassword) {
-    return password === confirmPassword;
-}
-
-module.exports = { esPar, contraseniasCoinciden };
